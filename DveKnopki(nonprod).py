87efdb199341eb7963ea8f996cd70fe7113d28c1
jjustping <SemenStar5544@gmail.com>
2024-02-17 13:40:39 +1100

    Update DveKnopki(nonprod).py

--- DveKnopki(nonprod).py
+++ DveKnopki(nonprod).py
@@ -1,11 +1,18 @@
-from PyQt6.QtWidgets import QApplication, QWidget
 import sys
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setWindowTitle("Главное меню")
+        self.setMinimumSize(QSize(600, 400))
+
 
 app = QApplication(sys.argv)
 
-# Виджет Qt — окно.
-window = QWidget()
-window.show()  # Важно: окно по умолчанию скрыто.
+window = MainWindow()
+window.show()
 
-# Запуска цикла событий.
 app.exec()
